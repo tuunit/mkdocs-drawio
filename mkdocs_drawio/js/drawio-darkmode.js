fc6ffc4c985bd0441bcfe0003bc258a5959a3625
Francisco Torres <ftorres7382@gmail.com>
2025-12-20 23:34:16 -0400

    Added support for full screen dark mode and no color flickering

--- mkdocs_drawio/js/drawio-darkmode.js
+++ mkdocs_drawio/js/drawio-darkmode.js
@@ -1,16 +1,2 @@
-const colorSchemeToggle = async function () {
-    document.querySelectorAll("div.mxgraph").forEach(function (div) {
-        let style = div.getAttribute("style").replace(/color-scheme: light;/gi, "");
-        div.setAttribute("style", style);
-    });
-};
-
-if (typeof document$ !== "undefined" && typeof document$.subscribe === "function") {
-    document$.subscribe(({ body }) => {
-        colorSchemeToggle();
-      });
-} else {
-      document.addEventListener("DOMContentLoaded", function () {
-        colorSchemeToggle();
-    });
-}
+// Historical code no longer needed.
+// Keeping file in case it is needed in the future. 
